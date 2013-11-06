5f65228cb0be6e10d39d4aa4aee5204f71a22344
deepblue <byblue@gmail.com>
2013-11-06 17:49:50 +0900

    remove slashes from header_id

--- ext/redcarpet/html.c
+++ ext/redcarpet/html.c
@@ -268,7 +268,7 @@ rndr_linebreak(struct buf *ob, void *opaque)
 char *header_id(const struct buf *text)
 {
 	VALUE str = rb_str_new2(bufcstr(text));
-	VALUE space_regex = rb_reg_new(" +", 2 /* length */, 0);
+	VALUE space_regex = rb_reg_new("[\\s/]+", 6 /* length */, 0);
 	VALUE tags_regex = rb_reg_new("<\\/?[^>]*>", 10, 0);
 
 	VALUE heading = rb_funcall(str, rb_intern("gsub"), 2, space_regex, rb_str_new2("-"));
